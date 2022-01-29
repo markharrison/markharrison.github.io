affe2524eee6da8b70818a8eb4a77130839d4acd
Mark Harrison <5177824+markharrison@users.noreply.github.com>
2022-01-29 22:06:24 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div >
-          <img src="https://github.com/markharrison.png" className={styles.profileImage}   />
+          <img alt="Mark Harrison" src="https://github.com/markharrison.png" className={styles.profileImage}   />
         </div>
       </div>
     </header>
